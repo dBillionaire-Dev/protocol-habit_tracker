2024923705b6e2c346930d27fda8314712a9fb78
dBillionaire-Dev <nezerekunke@gmail.com>
2026-03-22 07:32:10 +0100

    fix: clean next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,35 +1,74 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//     output: 'standalone',
+//     outputFileTracingRoot: __dirname,
+//   turbopack: {
+//     root: __dirname,
+//   },
+//   typescript: {
+//       ignoreBuildErrors: true,
+//   },
+//   experimental: {
+//       turbopack: false,
+//     serverActions: {
+//       bodySizeLimit: '2mb',
+//     },
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '**',
+//       },
+//     ],
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://localhost:5000/api/:path*',
+//       },
+//     ];
+//   },
+// };
+//
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-   outputFileTracingRoot: __dirname,
-  turbopack: {
-    root: __dirname,
-  },
-  typescript: {
-      ignoreBuildErrors: true,
-  },
-  experimental: {
-      turbopack: false,
-    serverActions: {
-      bodySizeLimit: '2mb',
+    reactStrictMode: true,
+
+    // Fix turbopack build issues
+    output: "standalone",
+
+    typescript: {
+        ignoreBuildErrors: true, // temporary
     },
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ];
-  },
+
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "2mb",
+        },
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: process.env.API_URL + "/api/:path*",
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
